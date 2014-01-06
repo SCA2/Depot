597352db905a61dfe1cc958b0aad3c3c8287fed2
Tim Ryan <tpryan_1965@yahoo.com>
2014-01-05 20:18:15 -0800

    Add cart code

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
+  
+  has_many  :line_items
+  
+  before_destroy  :ensure_not_referenced_by_any_line_item
+  
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
   validates :title, length: { minimum: 5 }
@@ -11,4 +16,17 @@ class Product < ActiveRecord::Base
   def self.latest
     Product.order(:updated_at).last
   end
+  
+  private
+  
+    #ensure that there are no  line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line items present')
+        return false
+      end
+    end
+    
 end
