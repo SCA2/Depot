d74a209c7576b99c09651587b6d131d1ed2b365c
Tim Ryan <tpryan_1965@yahoo.com>
2014-01-11 13:41:39 -0800

    More tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -27,12 +27,12 @@ class ProductTest < ActiveSupport::TestCase
     assert product.valid?
   end
 
-  test "title must be minimum 10 characters long" do
+  test "title must be minimum 5 characters long" do
     product = Product.new
-    product.title = "Short"
+    product.title = "1234"
     assert product.invalid?
-    assert_equal ["title must be at least 10 characters long"], product.errors[:title]
-    product.title = "Long Enough Title"
+    assert_equal ["title must be at least 5 characters long"], product.errors[:title]
+    product.title = "12345"
     assert product.valid?
   end
 
